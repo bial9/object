ebfa71e7e6619ef830726ed39811f74fc8749b5a
Bilal <99763904+bial9@users.noreply.github.com>
2022-06-08 00:45:24 +0500

    Add files via upload

---
+++ app.js
@@ -0,0 +1,28 @@
+//     function Std(firstName, lastName, age , subjects) {
+//   this.stdFirstName = firstName;
+//   this.stdLastName = lastName;
+//   this.stdAge = age;
+//   this.subjects = subjects;
+// }
+
+// var std1 = new Std("Bilal", "Asif", 19 , ["html" , "css"]);
+// console.log(std1);
+
+// var itemsArray = [
+// {name:"juice",price:50, quantity:3},
+// {name:"cookie",price:30, quantity:9},
+// {name:"shirt",price:880, quantity:1},
+// {name:"pen",price:100, quantity:2}];
+
+// var a = itemsArray[0].price;   
+// var b = itemsArray[1].price;   
+// var c = itemsArray[2].price;   
+// var d = itemsArray[3].price;   
+
+// console.log(a+b+c+d);
+
+// var e = itemsArray[0].quantity;   
+// var f = itemsArray[1].quantity;   
+// var g = itemsArray[2].quantity;   
+// var h = itemsArray[3].quantity; 
+// console.log(e+f+g+h);
